74980a4488a1092b93938929ff93d077fd90cbdd
Ajvni <ajaniqueeley@gmail.com>
2019-12-12 15:48:00 -0500

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,33 @@
 class BankAccount
 
-end
+    attr_reader :name
+    attr_accessor :balance, :status
+
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+    end
+
+    def deposit(deposit_total)
+        self.balance += deposit_total
+    end
+
+    # display_balance
+    # can display its balance (FAILED - 2)
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    # valid?
+    # is valid with an open status and a balance greater than 0 (FAILED - 3)
+    def valid?
+        self.status == "open" && self.balance > 0
+    end
+    # close_account
+    # can close its account (FAILED - 4)
+    def close_account
+        self.status = "closed"
+    end
+
+end # end of class
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,48 @@
 class Transfer
-  # your code here
+
+  attr_reader :sender, :receiver, :amount
+  attr_accessor :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"  
+  end
+
+  # valid?
+  # can check that both accounts are valid (FAILED - 2) 
+  # calls on the sender and receiver's #valid? methods (FAILED - 3)
+  def valid?
+  sender.valid? && receiver.valid?
+  end
+
+  # execute_transaction
+  # can execute a successful transaction between two accounts (FAILED - 4)
+  # each transfer can only happen once (FAILED - 5)
+  # rejects a transfer if the sender does not have enough funds (does not have a valid account) (FAILED - 6)
+  def execute_transaction 
+    if valid? && sender.balance > 0 && self.status = "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    else
+      self.status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
+
+
+  # reverse_transfer
+  # can reverse a transfer between two accounts (FAILED - 7)
+  # it can only reverse executed transfers (FAILED - 8)
+  def reverse_transfer
+    if valid? && receiver.balance > 0 && self.status = "complete"
+      sender.balance += amount
+      receiver.balance -= amount
+      self.status = "reversed" 
+    end
+
+  end
+
 end
